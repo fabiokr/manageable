7c74dbe36a3e66357665184896c135e1ba0a0730
Fabio Kreusch <fabiokr@gmail.com>
2012-02-28 08:14:09 -0300

    Added assets

---
+++ app/assets/javascripts/manageable/index.js
@@ -0,0 +1,8 @@
+//= require jquery
+//= require jquery-ui
+//= require jquery_ujs
+//= require ./jquery.scrollTo
+//= require ./jquery.localscroll
+//= require ./jquery.tipsy
+//= require ./jquery.uniform.min
+//= require ./datepicker
