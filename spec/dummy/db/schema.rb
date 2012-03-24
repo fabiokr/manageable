5ad66a8b9d9e95546ad79220336a7b3a611c134c
Fabio Kreusch <fabiokr@gmail.com>
2012-03-24 17:00:17 -0300

    Removed dummy migration

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120312112227) do
+ActiveRecord::Schema.define(:version => 20120311172544) do
 
   create_table "articles", :force => true do |t|
     t.string   "slug"
